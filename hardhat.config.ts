e9630aca45c354e142b133ff3e833e614b0b5a98
Ognjen Atlagic <atlagicognjen@gmail.com>
2022-06-27 15:47:52 +0200

    Added to gitignore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,6 @@ import "@nomiclabs/hardhat-waffle";
 import fs from "fs";
 
 const privateKey = fs.readFileSync(".secret").toString();
-const projectId = "b7dd9d8cb0564c538476aa4f11b4d012"; // this should go in .env
 
 module.exports = {
   defaultNetwork: "hardhat",
